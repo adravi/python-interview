94e793d3bbba2cf830a0f206336048d1bb47aa96
Sal Avila <sal.avila@outlook.com>
2023-05-23 15:34:29 -0700

    New problem added

---
+++ problems/arrays/easy/valid_subsequence.py
@@ -0,0 +1,21 @@
+# https://www.algoexpert.io/questions/validate-subsequence
+
+# input:
+# 1) array = [1, 2, 3, 4], sequence = [1, 3, 4]
+# 2) array = [5, 1, 22, 25, 6, -1, 8, 10], sequence = [1, 6, -1, 10]
+
+def isValidSubsequence(array, sequence):
+    i = 0
+    j = 0
+    
+    for i in range(len(array)):
+        if array[i] == sequence[j]:
+            j += 1
+
+        if j == len(sequence):
+            return True
+    
+    return False
+
+# O(n) T
+# O(1) S
