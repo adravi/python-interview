82a6ea097206cd9267515fee718e226f432188bf
Sal Avila <sal.avila@outlook.com>
2023-05-24 11:58:25 -0700

    Day3: Spiral Traverse

---
+++ problems/arrays/easy/contains_duplicate.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/contains-duplicate/description/
+
+# Given an integer array nums, return true if any value appears at least twice in the array,
+# return false if every element is distinct
+
+# Input: nums = [1,2,3,1]
+# Output: true
+
+def containsDuplicate(nums) -> bool:
+    existingNums = set()
+
+    for i in range(len(nums)):
+        num = nums[i]
+
+        if num in existingNums:
+            return True
+        else:
+            existingNums.add(num)
+    
+    return False
+
+# O(n) T
+# O(n) S
---
+++ problems/arrays/medium/spiral_traverse.py
@@ -0,0 +1,62 @@
+# array dimensions size are (n * m)
+# example: 4 * 4
+
+# [0,0] [0,1] [0,2] [0,3]
+# [1,0] [1,1] [1,2] [1,3]
+# [2,0] [2,1] [2,2] [2,3]
+# [3,0] [3,1] [3,2] [3,3]
+
+def spiralTraverse(array):
+    result = []
+
+    # Rows
+    startRow = 0
+    # (n - 1) = 3
+    endRow = len(array) - 1
+
+    # Columns
+    startCol = 0
+    # (m - 1) = 3
+    endCol = len(array[0]) - 1
+
+    # Because we defined the endRow and endCol as (dimensionSize - 1), we want to include the borders
+    while startRow <= endRow and startCol <= endCol:
+        # Remember the range(x) function does not include x !
+
+        # [0.0] [0,1] [0,2] [0,3]
+        for col in range(startCol, endCol + 1):
+            result.append(array[startRow][col])
+
+        # Check if array has a single row
+        if startRow == endRow:
+            break
+
+        # [1,3] [2,3] [3,3]
+        for row in range(startRow + 1, endRow + 1):
+            result.append(array[row][endCol])
+
+        # Check if array has a single col
+        if startCol == endCol:
+            break
+            
+        # [3,2] [3,1] [3,0]
+        for col in reversed(range(startCol, endCol)):
+            result.append(array[endRow][col])
+
+        # [2,0] [1,0]
+        for row in reversed(range(startRow + 1, endRow)):
+            result.append(array[row][startCol])
+
+        # Update row start and end -> Reduce the rectanlge to travel inwards
+        startRow += 1
+        endRow -= 1
+        # Update col start and end -> Reduce the rectanlge to travel inwards
+        startCol += 1
+        endCol -= 1
+
+    return result
+
+# O(n * m) T
+# O[n * m] S
+
+        
