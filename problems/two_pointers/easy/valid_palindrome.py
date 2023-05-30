9c7901b0fcd1f7b67ddc61a5358c52243864b4de
Sal Avila <sal.avila@outlook.com>
2023-05-30 16:37:06 -0700

    Day6: 2 pointers

---
+++ problems/two_pointers/easy/valid_palindrome.py
@@ -0,0 +1,34 @@
+
+def isValidPalindrome(s):
+    if len(s) == 0 or len(s) == 1:
+        return True
+    
+    leftPointer = 0
+    rightPointer = len(s) - 1
+
+    while leftPointer <= rightPointer:
+        leftChar = s[leftPointer]
+        rightChar = s[rightPointer]
+
+        if leftChar.isalnum() and rightChar.isalnum():
+            if leftChar.lower() != rightChar.lower():
+                return False
+            else:
+                leftPointer += 1
+                rightPointer -= 1
+
+        elif not leftChar.isalnum():
+            leftPointer += 1
+
+        elif not rightChar.isalnum():
+            rightPointer -= 1
+
+    return True
+
+
+# O(n) Time
+# O(1) Space
+
+isValidPalindrome("A man, a plan, a canal: Panama")
+isValidPalindrome('"race a car"')
+isValidPalindrome(' ')
