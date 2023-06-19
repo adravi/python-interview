a9880e316d91dc0e4dac4738bfc12f7ea03ac997
Sal Avila <sal.avila@outlook.com>
2023-06-18 21:47:30 -0700

    Trapping Rain Water. TwoPointers

---
+++ strategies/two_pointers/hard/trapping_rain_water.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/trapping-rain-water/
+# input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# output: 6
+# The above elevation map (black section) is represented by array [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+# In this case, 6 units of rain water (blue section) are being trapped // see input1
+
+# input: height = [4, 2, 0, 3, 2, 5]
+# output: 9 // see input2
+
+def trap(height):
+    pass
