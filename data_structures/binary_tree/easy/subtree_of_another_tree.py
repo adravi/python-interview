77b3b2a28431f1e242e09e283b4640f47047726d
Sal Avila <sal.avila@outlook.com>
2023-07-04 14:50:05 -0700

    Subtree of another tree. II

--- data_structures/binary_tree/easy/subtree_of_another_tree.py
+++ data_structures/binary_tree/easy/subtree_of_another_tree.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/subtree-of-another-tree/
+# input: tree of size T, subtree of size S
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -6,41 +7,31 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def isSubtree(self, root, subRoot):
-    def isSameTree(p, q):
-        stack = [[p, q]]
+# strategy: recursively explore the tree, 
+#           for any node matching the root of subtree recursively explore the rest of the tree to see if they match entirely
+def is_subtree(tree, subtree):
 
-        while stack:
-            p, q = stack.pop()
-
-            if (p and not q) or (not p and q):
-                return False
-
-            elif p and q and p.val != q.val:
-                return False
-
-            elif p and q and p.val == q.val:
-                stack.append([p.left, q.left])
-                stack.append([p.right, q.right])               
-        
+    if not subtree:              # any empty subtree is encompased in any given tree (even if the tree is empty)
+        return True
+    if not tree:                 # if a tree is empty, no non-empty subtree will be encompased in it
+        return False
+    
+    if same_tree(tree, subtree): # we know that tree and subtree are non-empty. Make the comparison starting from the roots
         return True
     
-    isSubTree = False
+    return (is_subtree(tree.left, subtree.left) or     # keep exploring to see if at least, one of the branches is the same
+            is_subtree(tree.right, subtree.right))
 
-    def dfs(node):
-        nonlocal isSubTree
 
-        if not node:
-            return
-        
-        if node.val == subRoot.val:
-            if isSameTree(node, subRoot):
-                isSubTree = True
-                return
-        
-        dfs(node.left)
-        dfs(node.right)
+def same_tree(t1, t2):            # helper function      
+    if not t1 and not t2:         # if both the tree nodes are empty, we can call them "the same"
+        return True
+    
+    if t1 and t2 and t1.val == t2.val:           # if the tree nodes are non-empty, compare their values
+        return (same_tree(t1.left, t2.left) and  # keep exploring to see if at least, one of the branches is the same
+                same_tree(t1.right, t2.right))
+    
+    return False                  # if we reach this point, the trees are NOT the same
 
-    dfs(root)
-
-    return isSubTree
+# O(T * S) time
+# O(1)     space
