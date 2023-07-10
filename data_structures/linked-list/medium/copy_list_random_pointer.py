52fbac1d0f1fe57ad5679078d125c3a8180c2b20
Sal Avila <sal.avila@outlook.com>
2023-07-10 11:13:54 -0700

    Notes of Copy Linked List with random pointer

--- data_structures/linked-list/medium/copy_list_random_pointer.py
+++ data_structures/linked-list/medium/copy_list_random_pointer.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/copy-list-with-random-pointer/
-
+# input: see image
 
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
@@ -7,20 +7,24 @@ class Node:
         self.next = next
         self.random = random
 
+# strat: Two passes, one for the copy-nodes creation, a 2nd one for the linking. Have a hashmap for accessing 
 def copy_random_list(head):
-    copies = { None: None }
+    copies = { None: None }   # have a (key:pair) element pointing to None, for edge cases of random pointing to None
 
-    curr = head
+    curr = head               # set the current node to the beginning of the list
     while curr:
-        copy = Node(curr.val)
-        copies[curr] = copy
-        curr = curr.next
+        copy = Node(curr.val)           # create the copy-node
+        copies[curr] = copy             # store the ref to the copy-node in the hashmap, using the curr node as key
+        curr = curr.next                # keep exploring the list
 
-    curr = head
+    curr = head               # reset the current node to the beginning of the list
     while curr:
-        copy = copies[curr]
-        copy.next = copies[curr.next]
-        copy.random = copies[curr.random]
-        curr = curr.next
+        copy = copies[curr]               # access the copy-node using the hashmap, and the curr node as key
+        copy.next = copies[curr.next]     # get next node by simply using curr.next as key, since all the copy-nodes are accessible
+        copy.random = copies[curr.random] # get random node by using curr.random as key, since all the copy-nodes are accessible
+        curr = curr.next                  # keep exploring the list
 
-    return copies[head]
+    return copies[head]     # simply return the head by getting it from the map, since all the copy-nodes are accessible
+
+# O(n) time  // O(2n) -> O(n)
+# O(n) space // hashmap aux mem
