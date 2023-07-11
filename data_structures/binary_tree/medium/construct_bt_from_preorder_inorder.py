dbceac944b6fd6c0e201d0c1711786a9ce1786d0
Sal Avila <sal.avila@outlook.com>
2023-07-11 12:30:53 -0700

    Notes. Construnct binay tree from preorder and inorder

--- data_structures/binary_tree/medium/construct_bt_from_preorder_inorder.py
+++ data_structures/binary_tree/medium/construct_bt_from_preorder_inorder.py
@@ -20,6 +20,8 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# strat: find the root in 'preorder', then locate the root in 'inorder', (the new root of a subtree is in the middle)
+#        then RECURSIVELY call the function with the 'trimmed' sublists, the left one represents the left sublist and so on...
 def build_tree(preorder, inorder):
     if not preorder or not inorder:
         return None
@@ -28,11 +30,19 @@ def build_tree(preorder, inorder):
     mid = inorder.index(root.val)   # [9, 3, 15, 20, 7]
                                     #     ^
                                     #    mid
+                                    # the left sublist represents the left subarray, and the right sublist the right subarray
 
-    root.left = build_tree(preorder[1 : mid + 1], inorder[:mid])
-    root.right = build_tree(preorder[mid + 1:], inorder[mid + 1:])
+    root.left = build_tree(preorder[1 : mid + 1], inorder[:mid])   # preorder: [not-include first one (root) : mid]
+                                                                   #                                  (right is not inclusive)
+                                                                   # inorder: [everything up to (mid-1)]
+
+    root.right = build_tree(preorder[mid + 1:], inorder[mid + 1:]) # preorder: [everything from one after the mid]
+                                                                   # inprder: [everything from mid+1]
 
     return root
+
+# O(log n) time
+# O(1) space
 
 
 
